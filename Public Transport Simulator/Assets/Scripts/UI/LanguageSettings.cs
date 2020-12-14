4763377ca386f14ea82f335dcd8d364f4f15aeff
alen-smajic <alensm@windowslive.com>
2020-12-15 00:42:44 +0100

    Update

---
+++ Public Transport Simulator/Assets/Scripts/UI/LanguageSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageSettings : MonoBehaviour
+{
+    [SerializeField] List<GameObject> UI_Eng;
+    [SerializeField] List<GameObject> UI_Ger;
+
+    public void EnglishUI()
+    {
+        foreach (var obj in UI_Ger)
+           obj.SetActive(false);
+
+        foreach (var obj in UI_Eng)
+           obj.SetActive(true);
+    }
+
+    public void GermanUI()
+    {
+        foreach (var obj in UI_Ger)
+            obj.SetActive(true);
+
+        foreach (var obj in UI_Eng)
+            obj.SetActive(false);
+    }
+}
